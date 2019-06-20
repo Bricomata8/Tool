4b4e37a9e93b61a7103b8ce16ca6304dbedcea83
aghiles <githubfortyuds@gmail.com>
2019-06-20 18:44:05 +0200

    commit

--- config/tfkolm/firefox/p2w7kwjh.default/browser-extension-data/languagetool-webextension@languagetool.org/storage.js
+++ config/tfkolm/firefox/p2w7kwjh.default/browser-extension-data/languagetool-webextension@languagetool.org/storage.js
@@ -1,1 +1,1 @@
-{"uniqueId":"a2c26c77fb7a9c92","firstVisit":1560934317,"sessionCount":25}
+{"uniqueId":"a2c26c77fb7a9c92","firstVisit":1560934317,"sessionCount":45,"allowRemoteCheck":true,"usageCount":24,"appliedSuggestions":2,"hiddenErrors":[{"day":"Thu Jun 20 2019","count":1}]}
